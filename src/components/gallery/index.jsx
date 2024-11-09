10098bee822ceee2ec571f8182039f4ad8119219
okafohenry <okafohenrie@gmail.com>
2024-11-09 12:10:10 +0100

    gallery section cont'd 2

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 import aiTech from '../../../public/ai-tech.svg';
+import anim1 from '../../../public/g-animation1.svg';
+import anim2 from '../../../public/g-animation2.svg';
+import anim3 from '../../../public/g-animation3.svg';
 
 export default function Gallery() {
 
@@ -8,7 +11,10 @@ export default function Gallery() {
     const absoluteBottom = 'absolute bottom-10 lg:text-[25px] text-[18px] ml-10 font-[400] text-white/[.8]'
 
     return (
-        <div className="bg-[#FAFAFA] lg:py-[160px] py-[100px]">
+        <div className="bg-[#FAFAFA] lg:py-[160px] py-[100px] relative">
+            <Image src={anim1} alt="anim1" className='absolute bottom-[9%] right-0 z-10 lg:w-auto w-[10%] animate-bounce' />
+            <Image src={anim2} alt="anim2" className='absolute top-[13%] lg:-left-10 z-10 lg:w-auto w-[10%] animate-bounce' />
+            <Image src={anim3} alt="anim3" className='absolute top-[60%] lg:-left-[3rem] z-10 lg:w-auto w-[10%] animate-pulse' />
             <div className="w-[90%] mx-auto">
                 <h1 className="lg:w-[65%] mx-auto lg:text-[75px] text-[45px] font-[600] text-center lg:leading-[100px] leading-[60px] text-[#0D0D0D]">Designed with You and Your Guests <span className="text-primary">in Mind</span></h1>
                 <p className="lg:w-[65%] mx-auto lg:text-[30px] text-center text-[20px] mt-8  text-[#0D0D0D]">Explore the features that will bring your big day to life, making your wedding celebration unforgettable</p>
@@ -35,7 +41,7 @@ export default function Gallery() {
                         <div className={`h-[400px] ${cardStyle} bg-5`}>
                             <div className='absolute top-10 ml-10'>
                                 <div className="flex items-center gap-x-3">
-                                    <Image src={aiTech} alt="ai" className='lg:w-auto w-[15%]' />
+                                    <Image src={aiTech} alt="ai" className='md:w-auto w-[15%]' />
                                     <p className={`lg:text-[25px] text-[18px] font-[400] text-white/[.8]`}>AI Assisted</p>
                                 </div>
                                 <h3 className={`lg:text-[37px] text-[27px]`}>Virtual Guestbook</h3>
@@ -45,12 +51,12 @@ export default function Gallery() {
                         <div className={`lg:h-[1250px] h-[400px] ${cardStyle} bg-6`}>
                             <div className='absolute top-10 ml-10'>
                                 <div className="flex items-center gap-x-3">
-                                    <Image src={aiTech} alt="ai" className='lg:w-auto w-[15%]' />
+                                    <Image src={aiTech} alt="ai" className='md:w-auto w-[15%]' />
                                     <p className={`lg:text-[25px] text-[18px] font-[400] text-white/[.8]`}>AI Assisted</p>
                                 </div>
                                 <h3 className={`lg:text-[37px] text-[27px]`}>Your Personalized Love Story,<br className='lg:block hidden' /> Beautifully Told</h3>
                             </div>
-                            <p className={`${absoluteBottom}`}>Where Your Wedding Moments Live On</p>
+                            <p className={`${absoluteBottom}`}>A Journey Worth Sharing</p>
                         </div>
                         <div className={`lg:h-[828px] h-[400px] ${cardStyle} bg-7`}>
                             <h3 className={`${absoluteTop}`}>Personalized Photo Album</h3>
