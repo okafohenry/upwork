c2526288b6e6000697036cdf85e571ea984b481e
okafohenry <okafohenrie@gmail.com>
2024-11-13 21:01:32 +0100

    more adjustments to responsiveness animations

--- src/components/access/Memorable.jsx
+++ src/components/access/Memorable.jsx
@@ -6,7 +6,7 @@ import memorable from '../../../public/memorable.svg'
 
 export default function Memorable() {
     return (
-        <div data-aos="fade-in" className=" lg:w-auto w-[17%] absolute lg:bottom-[6.5rem] bottom-[19rem] lg:right-[12%] right-[15%]">
+        <div data-aos="fade-in" className=" lg:w-auto w-[17%] absolute lg:bottom-[6.5rem] bottom-[9rem] lg:right-[12%] right-[16%]">
             <div className="relative left-[65%]">
                 <Image src={arrow} className="relative " />
                 <Image src={memorable} className="relative -right-[30%] top-4" />
