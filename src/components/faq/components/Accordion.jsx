7c6bbab712596b5633576813ac02e4c4365373c0
okafohenry <okafohenrie@gmail.com>
2024-11-15 12:52:12 +0100

    completed web view layout arrangement, next - view animations

--- src/components/faq/components/Accordion.jsx
+++ src/components/faq/components/Accordion.jsx
@@ -5,7 +5,7 @@ import closeIcon from '../../../../public/collapsible-closed.svg';
 
 export default function Accordion({ id, title, body, isOpen, setIsOpen }) {
   return (
-    <div className={`w-full lg:px-[25px] px-[15px] lg:py-[13px] py-[8px] rounded-[32px] bg-white/[.6] ${isOpen.status && isOpen.id === id ? 'shadow-md' : 'shadow-none'}`}>
+    <div className={`w-full lg:px-[25px] px-[15px] lg:py-[13px] py-[8px] rounded-[32px] ${isOpen.status && isOpen.id === id ? 'shadow-lg shadow-[#4F0D2540] bg-[#FDF0DB]' : 'shadow-none bg-[#FFE5E3BF]/[.75]'}`} onClick={setIsOpen}>
         <div className='flex items-center lg:gap-x-5 gap-x-1'>
             <Image 
             src={isOpen.status && isOpen.id === id ? openIcon : closeIcon } 
@@ -15,7 +15,7 @@ export default function Accordion({ id, title, body, isOpen, setIsOpen }) {
               transform: isOpen.status && isOpen.id === id ? 'rotate(360deg)' : 'rotate(0deg)',
             }}
             />
-            <h4 className='text-[#0D0D0D] lg:text-[25px] text-[16px] hover:cursor-pointer' onClick={setIsOpen}>{title}</h4>
+            <h4 className='text-[#4F0D25] lg:text-[25px] text-[16px] hover:cursor-pointer'>{title}</h4>
         </div>
         {isOpen.status && isOpen.id === id ? 
         <div className='lg:ml-[4.59rem] ml-[3.5rem] pb-2 mt-2 transition ease-in duration-300'>
--- src/components/get-started/index.jsx
+++ src/components/get-started/index.jsx
@@ -4,12 +4,12 @@ export default function GetStarted({ openModal }) {
         <div className="get-started-bg">
             <div className="w-[90%] mx-auto">
                 <div className="lg:w-[65%] w-[80%] grid gap-y-10 pt-[10rem]">
-                    <h1 data-aos="fade-up" className="lg:text-[85px] text-[50px] font-[600] lg:leading-[104px] leading-[60px]">Ready for an <span className="text-primaryLight">Unforgettable</span> Wedding?</h1>
+                    <h1 data-aos="fade-up" className="lg:text-[85px] text-[50px] font-[600] lg:leading-[104px] leading-[60px]">Ready for an <span className="text-[#FED4D0]">Unforgettable</span><br /> Wedding?</h1>
                     <p data-aos="fade-up" className="lg:text-[30px] text-[20px] lg:w-[85%] text-white">Begin your journey to a stress-free, beautifully organized celebration today with GatherGram</p>
                     <div data-aos="fade-up" className="lg:w-[40%] w-[75%] mt-10">
                         <button 
                         onClick={openModal}
-                        className="w-full text-center rounded-[50px] bg-white lg:text-[18px] text-[16px] text-primary font-[400] py-[18px]">
+                        className="w-full text-center rounded-[50px] bg-[#FD7E72] lg:text-[18px] text-[16px] text-white font-[400] py-[18px]">
                             Let’s get started!
                         </button>
                     </div>
